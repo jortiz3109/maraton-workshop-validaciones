0bb374ec69863403c67aba150190180522cc7990
John Edisson Ortiz <john.ortiz@evertecinc.com>
2022-01-29 18:00:06 -0500

    build: configured laravel mix to compile vue.js components

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .extract()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
